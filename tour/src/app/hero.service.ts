45ba86fb1c7fed0d0d857eeedfd1d5694667317f
ceie2100 <csoka.ervin12@gmail.com>
2019-05-05 11:27:34 +0300

    message & hero-manager service

---
+++ tour/src/app/hero.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Observable, of} from "rxjs";
+
+import { MessageService } from "./message.service";
+import { Hero } from "./hero";
+import { HEROES } from "./mock-heroes";
+
+// Marks the class as one that participates in dependency injection
+// @Injectable accepts a metadata object for the service
+// Make the service available to dependency injection -
+//  register a provider (can create or deliver a service) that instantiates HeroService
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  getHeroes(): Observable<Hero[]> {
+    //Send a message at fetch
+    this.messageService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+  // Angular will inject MessageService into the property
+  constructor(private messageService: MessageService) { }
+}
--- tour/src/app/heroes/heroes.component.ts
+++ tour/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
 import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,26 @@ import { HEROES } from "../mock-heroes";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor() { }
+  // Must be asynchronous - either take a callback, return a promise, return an Observable
+  // HttpClient.get() - returns an Observable (RxJS)
+  // Observable emits the array of heroes, subscribe passes the array to the callback which sets the heroes property
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
 
-  ngOnInit() { }
+  // When Angular creates a HeroesComponent, the Dependency Injection system sets
+  //  the heroService parameter to the singleton instance of HeroService.
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
 }
